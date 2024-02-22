ec9d1d3095360a8f97c4356ffe013ed8fa4228dd
Suhaib Kankodi <skankodi@iu.edu>
2024-02-22 05:22:36 +0000

    V4 Complete Clouf Function

--- gcf_generate_thumbnails/index.js
+++ gcf_generate_thumbnails/index.js
@@ -5,6 +5,11 @@ const fs = require('fs-extra');
 const os = require('os');
 const sharp = require('sharp');
 
+const getExif = require('exif-async');
+const parseDMS = require('parse-dms');
+
+const {Firestore} = require('@google-cloud/firestore');
+
 // Entry point function
 exports.generateThumbnail = async (file, context) => {
   const gcsFile = file;
@@ -38,7 +43,7 @@ exports.generateThumbnail = async (file, context) => {
   }
 
   // If the file is a valid photograph, download it to the 'local' VM so that we can create a thumbnail image
-  if (validFile) {
+  if (validFile) { 
     // Create a new filename for the 'final' version of the image file
     // The value of this will be something like '12745649237578595.jpg'
     const finalFileName = `${gcsFile.generation}.${fileExtention}`;
@@ -58,6 +63,14 @@ exports.generateThumbnail = async (file, context) => {
     await sourceBucket.file(gcsFile.name).download({
       destination: tempFilePath
     });
+
+    // Pass the LOCAL file to our readExifData function
+    const gpsObject = await readExifData(tempFilePath);
+    console.log(gpsObject);
+    const gpsDecimal = getGPSCoordinates(gpsObject);
+    console.log(gpsDecimal);
+    console.log(gpsDecimal.lat);
+    console.log(gpsDecimal.lon);
 
     // Upload our local version of the file to the final images bucket
     await finalBucket.upload(tempFilePath);
@@ -79,9 +92,53 @@ exports.generateThumbnail = async (file, context) => {
     // Delete the temp working directory and its files from the GCF's VM
     await fs.remove(workingDir);
 
+    // Write the dataObject to a Firestore document
+    const firestore = new Firestore({
+      projectId: "sp24-41200-skankodi-globaljags"
+    });
+
+   // Create a JavaScript object with necessary information about the photo
+    const dataObject = {};
+    dataObject.imageName = finalFileName;
+    dataObject.imageURL  = tempFilePath;
+    dataObject.latitude = gpsDecimal.lat;
+    dataObject.longitude = gpsDecimal.lon;
+    dataObject.thumbURL = thumbPath;
+
+    const collectionRef = firestore.collection('photos');
+    const documentRef = await collectionRef.add(dataObject);
+    console.log(`Document created: ${documentRef.id}`);
   } // end of validFile==true
 
   // DELETE the original file uploaded to the "Uploads" bucket
   await sourceBucket.file(gcsFile.name).delete();
   console.log(`Deleted uploaded file: ${gcsFile.name}`);
+}
+
+// Helper Functions
+async function readExifData(localFile) {
+  let exifData;
+  try{
+      exifData = await getExif(localFile);
+      //console.log(exifData);
+      //console.log(exifData.gps);
+      //console.log(exifData.gps.GPSLatitude);
+      return exifData.gps;
+  } catch(err) {
+      console.log(err);
+      return null;
+  }
+}
+
+function getGPSCoordinates(g) {
+  // P DMS needs string in exactly this format:
+  // D:M:SDIR D:M:SDIR
+  // 51:30:0.5486N 0:7:34.4503W
+  const latString = `${g.GPSLatitude[0]}:${g.GPSLatitude[1]}:${g.GPSLatitude[2]}${g.GPSLatitudeRef}`;
+  const lonString = `${g.GPSLongitude[0]}:${g.GPSLongitude[1]}:${g.GPSLongitude[2]}${g.GPSLongitudeRef}`;
+
+  // Use the parse-dms package to convert from DMS to decimal values
+  const degCoords = parseDMS(`${latString} ${lonString}`);
+
+  return degCoords;
 }
